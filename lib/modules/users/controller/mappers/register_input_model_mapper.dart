f452cbabb93511bbc5c04498926e8499e8a9b60b
Juliano Ventola <ventolajuliano@gmail.com>
2021-02-09 10:52:42 -0300

    feat: receive request and parse it to json

---
+++ lib/modules/users/controller/mappers/register_input_model_mapper.dart
@@ -0,0 +1,15 @@
+import 'package:dw3_pizza_delivery_api/modules/users/view_models/register_input_model.dart';
+
+class RegisterInputModelMapper {
+  final Map<String, dynamic> _requestMap;
+
+  RegisterInputModelMapper(this._requestMap);
+
+  RegisterInputModel mapper() {
+    return RegisterInputModel(
+      name: _requestMap['name'],
+      email: _requestMap['email'],
+      password: _requestMap['password'],
+    );
+  }
+}

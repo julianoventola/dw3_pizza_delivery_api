f25d0cd8e5a40014cb4a0b283624233639859e63
Juliano Ventola <ventolajuliano@gmail.com>
2021-02-09 18:57:10 -0300

    feat: ajusts builder

--- lib/application/config/service_locator_config.config.dart
+++ lib/application/config/service_locator_config.config.dart
@@ -1,9 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 
-import 'database_connection_configuration.dart';
 import '../database/database_connection.dart';
+import 'database_connection_configuration.dart';
 import '../database/i_database_connection.dart';
+import '../../repositories/user/i_user_repository.dart';
+import '../../services/user/i_user_service.dart';
+import '../../modules/users/controller/user_controller.dart';
+import '../../repositories/user/user_repository.dart';
+import '../../services/user/user_service.dart';
+
+/// adds generated dependencies
+/// to the provided [GetIt] instance
 
 GetIt $initGetIt(
   GetIt get, {
@@ -13,5 +27,9 @@ GetIt $initGetIt(
   final gh = GetItHelper(get, environment, environmentFilter);
   gh.factory<IDatabaseConnection>(
       () => DatabaseConnection(get<DatabaseConnectionConfiguration>()));
+  gh.lazySingleton<IUserRepository>(
+      () => UserRepository(get<IDatabaseConnection>()));
+  gh.lazySingleton<IUserService>(() => UserService(get<IUserRepository>()));
+  gh.factory<UserController>(() => UserController(get<IUserService>()));
   return get;
 }
--- lib/modules/users/controller/user_controller.g.dart
+++ lib/modules/users/controller/user_controller.g.dart
@@ -1,4 +1,10 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
 part of 'user_controller.dart';
+
+// **************************************************************************
+// ShelfRouterGenerator
+// **************************************************************************
 
 Router _$UserControllerRouter(UserController service) {
   final router = Router();

3b6c7b3e5a054112b78bd59681398a4fd2534ac8
Juliano Ventola <ventolajuliano@gmail.com>
2021-02-09 21:13:55 -0300

    feat: user model

---
+++ lib/application/entities/user.dart
@@ -0,0 +1,34 @@
+import 'dart:convert';
+
+class User {
+  final int id;
+  final String name;
+  final String email;
+  final String password;
+
+  User({this.id, this.name, this.email, this.password});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'email': email,
+      'password': password,
+    };
+  }
+
+  factory User.fromMap(Map<String, dynamic> map) {
+    if (map == null) return null;
+
+    return User(
+      id: map['id'] ?? 0,
+      name: map['name'] ?? '',
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory User.fromJson(String source) => User.fromMap(json.decode(source));
+}
